dd9bf8b22d058a1c625101f166fd549afff7661c
Max Görner <5477952+MaxG87@users.noreply.github.com>
2025-05-31 00:11:14 +0200

    RED: Add example of back-to-back with atomic

--- tests/test_interweave.py
+++ tests/test_interweave.py
@@ -149,6 +149,7 @@ def test_interweave_yields_all_events_eventually[T](
             ],
         ),
         ([(1, 2, 0), (3, 3, 0), (4, 5, 0)], [{(1, 2, 0)}, {(3, 3, 0)}, {(4, 5, 0)}]),
+        ([(1, 3, 0), (3, 3, 0), (3, 5, 0)], [{(1, 3, 0)}, {(1, 3, 0), (3, 3, 0)}, {(3, 5, 0)}]),
     ],
 )
 def test_interweave_works[T](elements: list[T], expected: list[set[T]]) -> None:
